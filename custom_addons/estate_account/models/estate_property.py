26fe120930a52b649989c10df4fece96b29a3ef9
John Muiruri <muiruri@navi.co.ke>
2025-01-06 17:08:59 +0300

    override action_sell_property method to generate invoice

--- custom_addons/estate_account/models/estate_property.py
+++ custom_addons/estate_account/models/estate_property.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 
 class EstateProperty(models.Model):
@@ -8,4 +9,33 @@ class EstateProperty(models.Model):
 
     def action_sell_property(self):
         # print("This is a test")
+        # if math.isclose(self.selling_price,  0.00):
+        
+        if self.selling_price == False or self.selling_price is None:
+            raise UserError("Selling price cannot be zero")
+        
+        records = []
+        invoice = {
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            # 'state': 'draft',
+            'amount_total': self.selling_price,
+            'date': fields.Date.today(),
+            'invoice_line_ids': [
+                fields.Command.create({
+                    'name': 'Property Sales',
+                    'quantity': 1,
+                    'price_unit': self.selling_price,
+                }),
+                fields.Command.create({
+                    'name': 'Sales Tax',
+                    'quantity': 1,
+                    'price_unit': 0.06 * self.selling_price + 100,
+                }),
+            ],
+        }
+
+        records.append(invoice)
+        self.env['account.move'].create(records)
+
         return super(EstateProperty, self).action_sell_property()
